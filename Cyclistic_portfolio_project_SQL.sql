b7bbe31439df2c88f6dc08027a9062ba45f59cb6
Kushal Chakrabarti <110999262+chakrabartikushal24@users.noreply.github.com>
2022-08-14 21:35:29 +0530

    A few changes related to new visualizations added

--- Cyclistic_portfolio_project_SQL.sql
+++ Cyclistic_portfolio_project_SQL.sql
@@ -111,7 +111,7 @@ SET day_of_the_week = DATENAME(WEEKDAY, started_at)
 
 SELECT member_casual, COUNT(member_casual) -- This query gives us the breakdown of total observations for customer type
 FROM cyclistic_year_data
-GROUP BY member_casual;
+GROUP BY member_casual
 
 
 -- to calculate the percentage from total observations of casual and member riders in our data
@@ -119,7 +119,7 @@ GROUP BY member_casual;
 SELECT member_casual, COUNT(*) as customer_type_count, 
 COUNT(*)*100.0/ SUM(COUNT(*)) over () AS customer_type_percentage
 FROM cyclistic_year_data
-GROUP BY member_casual;
+GROUP BY member_casual
 
 -- hence it is observed that 56.64% of the total rides started in the 1 year data is by members and 43.36% is by casual members
 
@@ -130,7 +130,7 @@ SELECT member_casual, rideable_type, COUNT(*) as ride_type_count,
 COUNT(*)*100.0/SUM(COUNT(*)) OVER () as ride_type_percentage
 FROM cyclistic_year_data
 GROUP BY rideable_type, member_casual
-ORDER BY rideable_type, member_casual;
+ORDER BY rideable_type, member_casual
 
 
 -- We also try to find the same data as above but by the hour of the day
@@ -139,7 +139,7 @@ SELECT member_casual, rideable_type, DATEPART(HOUR, started_at) AS time_of_day, 
 COUNT(*)*100.0/SUM(COUNT(*)) OVER () as ride_type_percentage
 FROM cyclistic_year_data
 GROUP BY DATEPART(HOUR, started_at), rideable_type, member_casual
-ORDER BY DATEPART(HOUR, started_at), rideable_type, member_casual;
+ORDER BY DATEPART(HOUR, started_at), rideable_type, member_casual
 
 --similarly we also try to find the data for each day of the week for the bike type for each user type
 
@@ -147,7 +147,7 @@ SELECT member_casual, rideable_type, day_of_the_week, COUNT(*) as ride_type_coun
 COUNT(*)*100.0/SUM(COUNT(*)) OVER () as ride_type_percentage
 FROM cyclistic_year_data
 GROUP BY day_of_the_week, member_casual, rideable_type
-ORDER BY day_of_the_week, member_casual, rideable_type;
+ORDER BY day_of_the_week, member_casual, rideable_type
 
 
 -- We also try to check the percentage of rides started on different days of the week for each customer type
@@ -157,7 +157,7 @@ COUNT(*)*100.0/ SUM(COUNT(*)) over () AS day_ride_percentage
 FROM cyclistic_year_data
 --WHERE day_of_the_week = 'Sunday'
 GROUP BY day_of_the_week, member_casual
-ORDER BY day_of_the_week, member_casual;
+ORDER BY day_of_the_week, member_casual
 
 
 -- now we want to check the rides initiated during the day by each customer type to find if there is any pattern that can be seen
@@ -166,8 +166,7 @@ SELECT member_casual, DATEPART(HOUR,started_at) AS hour_of_the_day, COUNT(*) AS 
 COUNT(*)*100.0/SUM(COUNT(*)) OVER () AS day_ride_percentage
 FROM cyclistic_year_data
 GROUP BY DATEPART(HOUR,started_at), member_casual
-ORDER BY DATEPART(HOUR,started_at), member_casual;
-
+ORDER BY DATEPART(HOUR,started_at), member_casual
 
 -- we also try to find the number of rides initiated by user types in each month to find seasonal difference if any
 
@@ -182,15 +181,16 @@ ORDER BY DATEPART(MONTH, started_at), member_casual
 
 SELECT member_casual, MAX(duration) as max_duration, MIN(duration) AS min_duration
 FROM cyclistic_year_data
-GROUP BY member_casual;
+WHERE duration > 0
+GROUP BY member_casual
 
 
 -- Now we will check the the average duration of ride by different user type
 
 --First to check the average ride duration and the standard deviation in duration for each user type
-SELECT member_casual, AVG(duration) as average_duration, STDEV(duration) AS standard_deviation
+SELECT member_casual, ROUND(AVG(duration),2) as average_duration, ROUND(STDEV(duration),2) AS standard_deviation
 FROM cyclistic_year_data
-GROUP BY member_casual;
+GROUP BY member_casual
 
 
 
@@ -210,6 +210,13 @@ FROM cyclistic_year_data
 GROUP BY DATEPART(HOUR, started_at), member_casual
 ORDER BY DATEPART(HOUR, started_at), member_casual;
 
+-- To check the average duration of the rides by different months for each user type
+
+SELECT member_casual, DATEPART(HOUR, started_at) AS time_of_day, AVG(duration) AS average_duration
+FROM cyclistic_year_data
+--WHERE duration <> 0 -- This part can be included to exclude 0 duration values from result but there was no difference in the results
+GROUP BY DATEPART(MONTH, started_at), member_casual
+ORDER BY DATEPART(MONTH, started_at), member_casual;
 
 
 -- Finally we try to group the data by longitues and latitudes belonging to start and end docking stations
@@ -219,30 +226,30 @@ FROM cyclistic_year_data
 GROUP BY start_lat, start_lng, member_casual
 ORDER BY count DESC; 
 
--- A lot of latitudes and longitudes have only one count and the total rows even after grouping the data by latitude and longitude is huge.
+-- too many starting longitue and latitudes have only a few ride count which is incresing the number of points on the map and making it difficult to analyse
 
--- we use a CTE in order to first group the data and find the total count against the groups, then filter those groups having a count of more than 1
-WITH station_location AS (                                        -- demonstration of a CTE
-SELECT member_casual, start_lat, start_lng, COUNT(*) AS count
-FROM cyclistic_year_data
-GROUP BY start_lat, start_lng, member_casual)
-
-SELECT SUM(count) as geograhical_data FROM station_location
-WHERE count > 1;
-
---This shows that out of total 5.9 mil rides, 4.33 mil was started from same latitude and longitude more than 1 time
--- we can use this data for a plot on a map to see the pattern of use by the two user types
-
--- again we will use a view in order to filter out all those groups having less than 2 count 
+-- again we will use a view in order to filter out all those groups having less than 100 count
 
 WITH station_location AS (
-SELECT member_casual, start_lat, start_lng, COUNT(*) AS count 
+SELECT member_casual, ROUND(start_lat,3) AS starting_lat, ROUND(start_lng,3) AS starting_lng, COUNT(*) AS count 
 FROM cyclistic_year_data 
 GROUP BY start_lat, start_lng, member_casual)
 
-SELECT * 
+SELECT *
 FROM station_location 
-WHERE count > 1 
+WHERE count > 100
+ORDER BY count DESC;
+
+-- we will do the same with the ending latitude and longitudes and plot the results on a map
+
+WITH station_location AS (
+SELECT member_casual, ROUND(end_lat,3) AS ending_lat, ROUND(end_lng,3) AS ending_lng, COUNT(*) AS count 
+FROM cyclistic_year_data 
+GROUP BY end_lat, end_lng, member_casual)
+
+SELECT *
+FROM station_location 
+WHERE count > 100
 ORDER BY count DESC;
 
 -- the results of the same are imported to excel and later to power BI for visualization
